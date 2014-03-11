9328defe20b9eed1efb991847e76614110b3b0fa
adbern <adbern@gmail.com>
2014-03-11 10:33:16 -0700

    update database

--- avina_app/settings.py
+++ avina_app/settings.py
@@ -78,9 +78,9 @@ TEMPLATE_DIRS = (
 #     'default' = dj_database_url.config()
 # }
 
-#DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
-DATABASES['default'] =  dj_database_url.config()
+# DATABASES['default'] =  dj_database_url.config()
 
 # DATABASES = {
 #     "default": {
